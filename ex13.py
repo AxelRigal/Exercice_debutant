8f5f8e4ac78702519cd207102effa374ee7383e6
AxelRigal <37696088+AxelRigal@users.noreply.github.com>
2020-07-07 01:38:59 +0200

    0707

---
+++ ex13.py
@@ -0,0 +1,6 @@
+i = 5
+tab = []
+while i < 16:
+    tab.append(i)
+    i += 1
+print (tab)
---
+++ ex15.py
@@ -0,0 +1,6 @@
+import random
+min = 1
+max = 6
+
+for _ in range(6):
+    print(random.randint(min, max))
