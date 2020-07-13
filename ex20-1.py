1365f1afdf88460c0fb871b6c78da41c6d6ef279
AxelRigal <axel.rigal@gmail.com>
2020-07-14 00:03:18 +0200

    1407

---
+++ ex20-1.py
@@ -0,0 +1,4 @@
+liste_01 = [1, 5, 6, 7, 9, 10, 11]
+liste_02 = [2, 3, 5, 7, 8, 10, 12]
+liste_03 = [value for value in liste_01 if value in liste_02]
+print(liste_03)
---
+++ ex26.py
@@ -0,0 +1,2 @@
+from constants import nom
+print(nom)
